8ead5a9f1d2e21ac200fd0bff26737c4838c5e37
anikdev2016 <anikdev2016@gmail.com>
2023-01-26 14:52:57 +0600

    completed navbar primarily

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./SharedPages/Navbar/Navbar";
+import SubNavbar from "./SharedPages/SubNavbar/SubNavbar";
 
 const Main = () => {
   return (
     <div>
+      {/* navbar */}
+      <SubNavbar />
+      <Navbar />
+      {/* content*/}
       <Outlet />
+      {/* footer */}
     </div>
   );
 };
