8725ddbaf92901f3e5786e470e09f6cab1bce438
anikdev2016 <anikdev2016@gmail.com>
2023-01-26 22:15:03 +0600

    completed adding aos

--- src/Pages/Homepage/Benefits/Benefits.js
+++ src/Pages/Homepage/Benefits/Benefits.js
@@ -5,6 +5,8 @@ const Benefits = () => {
   const dummyArr = [1, 2, 3, 4];
   return (
     <div
+      data-aos="fade-up"
+      data-aos-duration="2000"
       style={{ boxShadow: "0px 16px 21px -9px grey" }}
       className="grid w-[95%] p-4  mx-auto rounded-md sm:gap-2 lg:gap-0 md:grid-cols-2 lg:grid-cols-4"
     >
